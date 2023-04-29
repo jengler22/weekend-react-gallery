4cd02e6559bb700e6f8b2b671ccb82019843fbf4
Jakob Engler <jengler2291@gmail.com>
2023-04-29 12:46:35 -0500

    added more mui

--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,26 +1,29 @@
 import React, { useState, useEffect } from "react";
 import GalleryItem from "../GalleryItem/GalleryItem.jsx";
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
 
 
 
 function GalleryList ({ myGalleryList, getItem}) {
     // lopping over arrray of photos
-    return(
-        <div>
-        <h2>Gallery List</h2>
-        {/* {JSON.stringify(myGalleryList)} */}
-        <div>
-        {myGalleryList.map((item) => (
-            <GalleryItem 
-                key={item.id} 
-                item={item} 
-                getItem={getItem}
-            />
-        ))}
-        </div>
-    </div>
-    );
-}
+    return (
+        // xl = Max 3 images wide
+        <Container maxWidth='xl'>
+            <Grid container spacing={0} justifyContent='center'>
+                {
+                    myGalleryList.map((item) => (
+                        <GalleryItem
+                            key={item.id}
+                            item={item}
+                            getItem={getItem}
+                        />
+                    ))
+                }
+            </Grid>
+        </Container>
 
+    )
+}
 
 export default GalleryList;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,15 @@
+import { AppBar, Toolbar, Typography } from '@mui/material';
 
-
-function Header () {
-
-    return(
-        <>
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-      </header>
-        </>
-    )
+function Header() {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'center' }}>
+          Gallery of My Life
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
 }
+
 export default Header;
